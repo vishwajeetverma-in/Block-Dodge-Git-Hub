da075b6df894e41e1b34e385a86b016c2bd58ffe
vishwajeetverma-in <vishwa11126@gmail.com>
2023-12-26 18:38:34 +0530

    GameManager script | refrence variable creation for :-  prefabbed block for spawning | spawnPoint empty gameobject for refrence of spawnpoint transform

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject block;
+    public Transform spawnPoint;
     // Start is called before the first frame update
     void Start()
     {
