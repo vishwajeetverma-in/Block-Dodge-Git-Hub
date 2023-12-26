09740964c7829533cbb5b3679768695b4e3cf93c
vishwajeetverma-in <vishwa11126@gmail.com>
2023-12-26 17:30:48 +0530

    ScreenToWorldPoint | mouseposition.x | screen partitioning  | Input System Applied

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();   
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetMouseButton(0))
+        {
+            Vector2 touchPos = Camera.main.ScreenToViewportPoint ( Input.mousePosition); // Screen gets partitioned at centre is 0 ,left is negative, right is positive
+            if(touchPos.x < 0)
+            {
+
+            }
+           
+
+
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+
+        }
         
     }
 }
